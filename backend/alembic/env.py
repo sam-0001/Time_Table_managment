c439becb9d2fef47f83ce0deef252dfb2c663a26
Soham Chaudhari <sohamchaudhari@Sohams-MacBook-Air.local>
2026-07-27 00:40:59 +0530

    Move app to root for vercel bundler

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -7,8 +7,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-# Add the parent directory to sys.path
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Add the root directory to sys.path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 from app.db.database import Base
 import app.db.models # Ensure models are loaded
--- backend/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Ensure the 'backend' directory is in the Python path
-# so that 'from app...' imports work correctly.
-sys.path.insert(0, os.path.dirname(__file__))
-
-from app.main import app
